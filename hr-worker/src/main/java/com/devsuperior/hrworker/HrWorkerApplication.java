85c232a54fb884c16784996e350640680127e20f
Luis Ricardo Teixeira Fernandes <lrtf.developer@gmail.com>
2023-06-17 18:26:18 -0300

    git private repository

--- hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
+++ hr-worker/src/main/java/com/devsuperior/hrworker/HrWorkerApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class HrWorkerApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(HrWorkerApplication.class, args);
 	}
