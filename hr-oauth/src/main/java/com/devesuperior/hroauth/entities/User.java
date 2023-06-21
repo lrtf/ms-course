f6a4c2185209ccbea50bda352ca7a75e7e77191d
Luis Ricardo Teixeira Fernandes <lrtf.developer@gmail.com>
2023-06-21 17:21:20 -0300

    Login, JWT token

--- hr-oauth/src/main/java/com/devesuperior/hroauth/entities/User.java
+++ hr-oauth/src/main/java/com/devesuperior/hroauth/entities/User.java
@@ -1,25 +1,30 @@
 package com.devesuperior.hroauth.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.stream.Collectors;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User implements Serializable {
-	
+public class User implements UserDetails, Serializable {
+
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
 	private String name;
 	private String email;
 	private String password;
-	private Set<Role> roles = new HashSet<>();	
-	
+	private Set<Role> roles = new HashSet<>();
+
 	public User() {
-		
+
 	}
-	
+
 	public User(Long id, String nome, String email, String password) {
 		this.id = id;
 		this.name = nome;
@@ -58,7 +63,7 @@ public class User implements Serializable {
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
+
 	public Set<Role> getRoles() {
 		return roles;
 	}
@@ -79,12 +84,37 @@ public class User implements Serializable {
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
-	
-	
-	
-	
-	
-	
-	
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(r -> new SimpleGrantedAuthority(r.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 
 }
--- hr-oauth/src/main/java/com/devesuperior/hroauth/services/UserService.java
+++ hr-oauth/src/main/java/com/devesuperior/hroauth/services/UserService.java
@@ -4,19 +4,23 @@ package com.devesuperior.hroauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.devesuperior.hroauth.entities.User;
 import com.devesuperior.hroauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 	
 	private static Logger logger = LoggerFactory.getLogger(UserService.class);
 	
 	@Autowired
 	private UserFeignClient userFeignClient;
 	
+	//Só por questões didaticas
 	public User findByEmail(String email) {
 		User user = userFeignClient.findByEmail(email).getBody();
 		if(user == null) {
@@ -24,6 +28,17 @@ public class UserService {
 			throw new IllegalArgumentException("Email not found");
 		}
 		logger.info("Email found: "+ email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userFeignClient.findByEmail(username).getBody();
+		if(user == null) {
+			logger.error("Email not foud: " + username);
+			throw new UsernameNotFoundException("Email not found");
+		}
+		logger.info("Email found: "+ username);
 		return user;
 	}
 
